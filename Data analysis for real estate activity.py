4deb8aaf41b7e35425270265c68c3dfada8f8f7f
Alexander Cox <124589421+alexcox121@users.noreply.github.com>
2023-04-18 19:59:01 +0100

    Add files via upload

---
+++ Data analysis for real estate activity.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#practice data analysis
+
+print("My name is Alexander Cox")
+print("The date is 18th April")
+
+#write a program to analyse real estate data
+
+#create variables, python doesn't use commas to separate thousands
+
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+
+# Total sales is sum of variables
+total_sales = a + b + c + d + e
+
+#view the output
+print(total_sales)
+
+
+# In[2]:
+
+
+#calculate average sales price for the month of decemeber
+#create a new variable
+#calculate the average sales by dividing the total number of sales by 5
+
+avg_sales = total_sales/5
+
+#view the output
+print(avg_sales)
+
+
+# In[3]:
+
+
+#print the property price for property C
+#create a new variable
+#calculate the price of property C as sales - commission
+
+property_price_c = (67000-6500)
+
+#view the output
+print(property_price_c)
+
+
+# In[ ]:
+
+
+
+
