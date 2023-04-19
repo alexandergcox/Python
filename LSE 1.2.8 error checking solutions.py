a8627eca290eebd9eb220a0c8b5fe2078de007c0
Alexander Cox <124589421+alexcox121@users.noreply.github.com>
2023-04-19 19:09:31 +0100

    Add files via upload

---
+++ LSE 1.2.8 error checking solutions.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Print the price.
+sales_price = 24500
+
+print(sales_price)
+
+
+# In[3]:
+
+
+# Print a text string verbatim.
+print("My name is James Bond")
+
+
+# In[5]:
+
+
+# Determine whether x is greater than 10.
+x = 11 
+
+if x > 10:
+    print('X is greater than 10')
+else: 
+    print('x is not greater than 10')
+
+
+# In[6]:
+
+
+# Create the variable list_a.
+list_a = [1,2,3,4,'Ayaan', 'Hirsi']
+
+print(list_a)
+
+
+# In[ ]:
+
+
+
+
